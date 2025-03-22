6d6eafb1b041eb25a4d67798636363b049184add
Rin0n <wnatthis1234@gmail.com>
2025-03-22 11:59:41 +0500

    Code itself and other components
    Code for creating complains and suggestions

---
+++ global-warming/speach.py
@@ -0,0 +1,21 @@
+import speech_recognition as speech_recog
+
+def speach_ru():
+    mic = speech_recog.Microphone()
+    recog = speech_recog.Recognizer()
+
+    with mic as audio_file:
+
+        recog.adjust_for_ambient_noise(audio_file)
+        audio = recog.listen(audio_file)
+    return recog.recognize_google(audio, language="ru-RU")
+
+def speach_en():
+    mic = speech_recog.Microphone()
+    recog = speech_recog.Recognizer()
+
+    with mic as audio_file:
+
+        recog.adjust_for_ambient_noise(audio_file)
+        audio = recog.listen(audio_file)
+    return recog.recognize_google(audio, language="en-EN")
